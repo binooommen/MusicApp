16ac8c563fff5d2c0d32ae6e1dea10bd0a16939f
Bino Oommen Samuel <binooommen37@gmail.com>
2015-04-02 22:27:02 -0400

    string added

--- gen/com/example/musicapp/R.java
+++ gen/com/example/musicapp/R.java
@@ -50,7 +50,10 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int imgDesc=0x7f060005;
         public static final int inital_Time=0x7f060003;
+        public static final int songDuration=0x7f060006;
+        public static final int songName=0x7f060004;
     }
     public static final class style {
         /** 
